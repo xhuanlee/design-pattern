fd0ac0feb6ae8c828849a92a08d3ab6fb58763ce
xhuanlee <lixhuan822@163.com>
2019-04-17 09:48:07 +0800

    add observer.md

---
+++ src/main/java/com/lxh/observer/WeatherData.java
@@ -0,0 +1,27 @@
+package com.lxh.observer;
+
+/**
+ * @author lixianghuan@allcomchina.com
+ * @date 2019/4/17 09:33
+ * @extra code change the world
+ * @description
+ */
+public class WeatherData {
+
+    public float getTemperture() {
+        return 0.0F;
+    }
+
+    public float getHumidity() {
+        return 0.0F;
+    }
+
+    public float getPressure() {
+        return 0.0F;
+    }
+
+    public void measuermentsChanged() {
+
+    }
+
+}
